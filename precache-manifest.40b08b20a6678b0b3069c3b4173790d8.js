44b50fa2f753acf23ec8a6699bf8b9c8497e3b2e
Andres Arana <ajoelarana@gmail.com>
2019-01-03 01:50:43 -0500

    Updates

---
+++ precache-manifest.40b08b20a6678b0b3069c3b4173790d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e51b96eba8031cddd19f",
+    "url": "/static/js/main.e51b96eb.chunk.js"
+  },
+  {
+    "revision": "d3b35bef97574aa74e72",
+    "url": "/static/js/1.d3b35bef.chunk.js"
+  },
+  {
+    "revision": "e51b96eba8031cddd19f",
+    "url": "/static/css/main.40b2898e.chunk.css"
+  },
+  {
+    "revision": "97829bd106a2d3a14a9f5a44ca6fd0f1",
+    "url": "/index.html"
+  }
+];
